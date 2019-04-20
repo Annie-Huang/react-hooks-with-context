a6359880920769ec8b1dfd9751dae8e559c54c50
Annie Huang <annie.hn@gmail.com>
2019-04-20 13:32:36 +1000

    Show how to call 'useContext' within nested components. Much more clean

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useContext } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const UserContext = React.createContext();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    state = {
+        firstName: "Dave",
+        lastName: "Thomas"
+    };
+
+    render() {
+        const { firstName, lastName } = this.state;
+
+        return (
+            <UserContext.Provider value={{ firstName, lastName }}>
+                <Main />
+            </UserContext.Provider>
+        );
+    }
+}
+
+function Navbar() {
+    const { firstName, lastName } = useContext(UserContext);
+
+    return (
+        <nav>
+            <span className="title">Cool App</span>
+
+            <span>
+        Hello, {firstName} {lastName}!
+      </span>
+        </nav>
+    );
+}
+
+function Main() {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <h2>App Title</h2>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+                    aspernatur quos minima, fuga quidem harum consequatur tempora animi
+                    omnis laborum tenetur, quod dignissimos quis. Ea optio eos expedita
+                    earum excepturi.
+                </p>
+            </main>
+        </>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
